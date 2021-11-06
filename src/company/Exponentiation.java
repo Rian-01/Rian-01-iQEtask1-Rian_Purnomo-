7f34192669271ece57c7e0c59763ba6758cf2aea
Rian1005 <rianpurnomo@gmail.com>
2021-11-06 05:04:12 -0400

    Basic Programming Part B

---
+++ src/company/Exponentiation.java
@@ -0,0 +1,19 @@
+package company;
+
+public class Exponentiation {
+    public static void main(String[] args){
+
+        int a = (int) Math.pow(2, 3);
+        int b = (int) Math.pow(5, 3);
+        int c = (int) Math.pow(10, 2);
+        int d = (int) Math.pow(2, 5);
+        int e = (int) Math.pow(7, 3);
+
+        //Output
+        System.out.println("Exponen dari 2, 3 adalah : " + a);
+        System.out.println("Exponen dari 5, 3 adalah : " + b);
+        System.out.println("Exponen dari 10, 2 adalah : " + c);
+        System.out.println("Exponen dari 2, 5 adalah : " + d);
+        System.out.println("Exponen dari 7, 3 adalah : " + e);
+    }
+}
